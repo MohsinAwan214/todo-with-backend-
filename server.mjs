a6ee52fec21476030215d969d263533758a96649
MohsinAwan214 <mohsinawan2267@gmai.com>
2025-01-04 10:42:52 +0500

    Add changes

--- server.mjs
+++ server.mjs
@@ -2,42 +2,68 @@ import express from "express";
 // import cors from 'cors'
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use(express.json())
-const todos = []
+const todos = [];
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send('testing todos');
-});
-
-
+// app.get("/", (req, res) => {
+//   res.send('testing todos');
+// });
 
 app.get("/get-all-todos", (req, res) => {
   res.send(todos);
 });
 
-
 // ye naya todo add krny ke ley he
 app.post("/add-todo", (req, res) => {
+  todos.push({todoContent : req.body.todo , id : String(new Date().getTime())});
 
-  todos.push(req.body.todos)
-
-  res.send("todo add ho gaya")
-
- });
+  res.send("todo add ho gaya");
+});
 
 // ye todo ko upDate ya edit krny ke ley he
-app.patch("/edit-todo/:id", (req, res) => { });
+app.patch("/edit-todo/:id", (req, res) => {
+
+  const id = req.params.id
+  console.log("ye he id", id);
+  
+const isFound = false
+
+
+for (let i = 0; i < todos.length; i++) {
+  if (todos[i].id === id) {
+    // idher product nil chuka he ab us product ko edit krna he
+    todos[i].todoContent = req.body.todoContent
+
+    isFound = true
+  }
+  
+}
+
+if(isFound){
+  res.status(201).send("todo updated successfully!")
+  
+}else{
+  
+  res.status(200).send('todo notfound')
+}
+
+});
+
+
+
+
+
+
+
+
 
 // ye todo ko elete krny ke ley he
-app.delete("/delete-todo/:id", (req, res) => { });
-
-
-
+app.delete("/delete-todo/:id", (req, res) => {});
 
 app.use("/", (req, res) => {
-  res.status(404).send('No route found');
+  res.status(404).send("No route found");
 });
 
 app.listen(port, () => {
